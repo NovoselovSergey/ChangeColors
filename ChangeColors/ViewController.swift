682b2749bd9a58f879228bd0b58f0e05e2684468
Novoselov Sergey <s.m.novoselov@icloud.com>
2019-07-28 00:16:10 +0300

    Initial Commit

---
+++ ChangeColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChangeColors
+//
+//  Created by Sergey Novoselov on 28/07/2019.
+//  Copyright © 2019 Sergey Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
